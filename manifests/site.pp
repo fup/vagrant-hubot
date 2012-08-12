3c391e753a1daeebaf450cf4592b542161592d2a
James Fryman <james@frymanet.com>
2012-08-12 02:44:03 -0500

    adding mount for scripts

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,9 +16,10 @@ node /hubot/ {
   class { 'nodejs_squeeze': }
   -> class { 'git': }
   -> class { 'hubot':
-      adapter => 'irc',
-      irc_nickname => 'crunchy',
-      irc_server   => $::ipaddress,
-      irc_rooms    => ['#soggies'],
+      adapter       => 'irc',
+      irc_nickname  => 'crunchy',
+      irc_server    => $::ipaddress,
+      irc_rooms     => ['#soggies'],
+      vagrant_hubot => true,
    }
 }
